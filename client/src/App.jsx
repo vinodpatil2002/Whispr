dbd79dbc509ad8a00eb65b1fc33c64a704a434d2
vinodpatil2002 <vinodnspatil@gmail.com>
2024-03-16 02:56:37 +0530

    Add lazy loading and routing to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-// import React from 'react'
+import React,{lazy} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import Home from './pages/Home'
+
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Chat = lazy(() => import('./pages/Chat'))
+const Groups = lazy(() => import('./pages/Groups'))
 
 function App() {
   return (
-    <div>APP</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/Groups" element={<Groups />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
